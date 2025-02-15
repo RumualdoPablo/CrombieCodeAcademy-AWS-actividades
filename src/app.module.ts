a158aea3989ec60ccfbae838518ed465ed924964
Emanuel <emanuel.villanueva.ext@puma.com>
2025-02-15 11:36:47 -0300

    actividades tercera clase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, ProductsModule, ItemsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
