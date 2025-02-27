aa5fa6c9d8894afc821a7586c18245a4f3c10f41
Emanuel <emanuel.villanueva.ext@puma.com>
2025-02-27 11:14:36 -0300

    cognito auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { ProductsModule } from './products/products.module';
 import { ItemsModule } from './items/items.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { CognitoAuthController } from './cognito-auth/cognitoAuth.controller';
+import { CognitoAuthModule } from './cognito-auth/cognitoAuth.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, ItemsModule, UserModule, AuthModule],
+  imports: [
+    PrismaModule,
+    ProductsModule,
+    ItemsModule,
+    UserModule,
+    AuthModule,
+    CognitoAuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
